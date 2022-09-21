af5ac0ba58d4e04ea0d573524cb6b037ab66a8c1
19083522 <ssd5717@autuni.ac.nz>
2022-09-21 21:33:15 +1200

    Revert "Merge branch 'main' into Liam"
    This reverts commit 78e62932002920698705bf4a55e3f5f6219b05de.

--- PermaGreed/Assets/Scripts/CurrentGun.cs
+++ PermaGreed/Assets/Scripts/CurrentGun.cs
@@ -3,35 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//this script acts as as the representation of the player's current gun on the Upgrades Menu.
 public class CurrentGun : MonoBehaviour
 {
     Sprite currentGun;
 
     void Start()
     {
-        //This start menu assigns a default gun to the start menu, which is representative of the gun the player is currently
-        //holding. when the currency and enemy death scripts have been implemented, this will be modified to instead pull the 
-        //gun the player is currently holding. If the player is not currently holding a gun, this will default to 
-        //the revolver preset laid out below.
+        if (GameData.gunName == null)
+        {
+            GameData.gunName = "Revolver";
+        }
 
-        GameData.gunName = "Revolver";
-        GameData.ammoCount = 6;
-        GameData.damage = 10;
-        GameData.range = 50;
-        GameData.reloadDuration = 2;
-        GameData.fireRate = 60;
-        GameData.gunRarity = GunStats.Rarity.Common;
+        Debug.Log(GameData.gunName);
 
-        //This as well as the values above call from the static script GameData, which houses all data that needs to be transferred between scenes
-        //for the game.
         currentGun = Resources.Load<Sprite>(GameData.gunName);
+
+        if (currentGun == null)
+        {
+            Debug.Log("There is no gun loaded");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //This will pull the gun the player is currently holding from the assigned currentGun sprite and assigning it to this image's sprite.
         this.gameObject.GetComponent<Image>().sprite = currentGun;
     }
 }
