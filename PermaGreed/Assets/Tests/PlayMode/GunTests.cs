a10d0c8257d5433aa00d016f0403d1991a295c72
SylvCrane <104751218+SylvCrane@users.noreply.github.com>
2022-10-20 23:42:30 +1300

    Unit tests
    Implemented unit testing

--- PermaGreed/Assets/Tests/PlayMode/GunTests.cs
+++ PermaGreed/Assets/Tests/PlayMode/GunTests.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEditor;
 
 public class GunTests
 {
-
     [UnityTest]
     public IEnumerator AmmoDecreases()
     {
-        var GameObject = new GameObject();
-       // var Player = GameObject.AddComponent<Player>();
-        yield return null;
+        GameObject gun = new GameObject();
+        GameObject gunParent = new GameObject();
+        gun = Object.Instantiate(Resources.Load("RevolverCommon")) as GameObject;
+        GameObject player = Object.Instantiate(Resources.Load("Player")) as GameObject;
+
+        gun.gameObject.transform.parent = gunParent.transform;
+        gun.gameObject.GetComponent<CollectScript>().player = player.transform;
+        gun.gameObject.GetComponent<DefaultGun>().tempAmmo = 6.0f;
+        Debug.Log(gun.gameObject.GetComponent<DefaultGun>().tempAmmo);
+        gun.SetActive(true);
+
+        yield return new WaitForSeconds(3);
+
+        gun.gameObject.GetComponent<DefaultGun>().Shoot();
+
+        Assert.AreEqual(5.0f, gun.gameObject.GetComponent<DefaultGun>().tempAmmo);
+
     }
 }
