7769e834a5d21ec91b01e04196a29754c2c9f1e6
SylvCrane <104751218+SylvCrane@users.noreply.github.com>
2022-10-20 16:39:59 +1300

    Gun additions

---
+++ PermaGreed/Assets/Tests/PlayMode/GunTests.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class GunTests
+{
+
+    [UnityTest]
+    public IEnumerator AmmoDecreases()
+    {
+        var GameObject = new GameObject();
+       // var Player = GameObject.AddComponent<Player>();
+        yield return null;
+    }
+}
