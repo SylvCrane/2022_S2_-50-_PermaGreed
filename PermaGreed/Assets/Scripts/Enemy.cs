b566d808a7b3ac59ee0e95309fae964ef8d1d37d
SylvCrane <104751218+SylvCrane@users.noreply.github.com>
2022-10-22 15:32:05 +1300

    Finalizing guns - part 1
    Finalizing the size of guns when they are picked up and the spawning of guns

--- PermaGreed/Assets/Scripts/Enemy.cs
+++ PermaGreed/Assets/Scripts/Enemy.cs
@@ -46,81 +46,348 @@ public class Enemy : MonoBehaviour
     public int gunRarity() //this will be subjected to heavy change during iteration 2
     {
         int rarity = Random.Range(0, 101);
-        int typeOfGun = Random.Range(1, 4);
+        int typeOfGun = Random.Range(1, 13);
         int choosenGun = 0;
 
         if (rarity >= 0 && rarity <= 50) //common
         {
-            if(typeOfGun == 1)
-            {
-                choosenGun = 0;
-            }
-
-            if(typeOfGun == 2)
-            {
-                choosenGun = 4;
-            }
-
-            if(typeOfGun == 3)
-            {
-                choosenGun = 8;
-            }
+            choosenGun = commonSelector(typeOfGun);
         }
-
         else if(rarity > 50 && rarity <= 80) //uncommon
         {
-            if (typeOfGun == 1)
-            {
-                choosenGun = 1;
-            }
-
-            if (typeOfGun == 2)
-            {
-                choosenGun = 5;
-            }
-
-            if (typeOfGun == 3)
-            {
-                choosenGun = 9;
-            }
+            choosenGun = uncommonSelector(typeOfGun);
         }
-
         else if (rarity > 80 && rarity <= 99) //rare
         {
-            if (typeOfGun == 1)
-            {
-                choosenGun = 2;
-            }
-
-            if (typeOfGun == 2)
-            {
-                choosenGun = 6;
-            }
-
-            if (typeOfGun == 3)
-            {
-                choosenGun = 10;
-            }
+            choosenGun = rareSelector(typeOfGun);
         }
-
         else
         {
-            if (typeOfGun == 1)
-            {
-                choosenGun = 3;
-            }
+            int legendaryChange = Random.Range(0, 100);
 
-            if (typeOfGun == 2)
+            if (legendaryChange == 99)
             {
-                choosenGun = 7;
+                choosenGun = legendarySelector(typeOfGun);
             }
-
-            if (typeOfGun == 3)
+            else
             {
-                choosenGun = 11;
+                choosenGun = epicSelector(typeOfGun);
             }
         }
 
         return choosenGun;
     }
+
+    public int commonSelector(int typeOfGun)
+    {
+        int choosenGun = 0;
+
+        //Pistol
+        if (typeOfGun == 1)
+        {
+            choosenGun = 0;
+        }
+
+        if (typeOfGun == 2)
+        {
+            choosenGun = 4;
+        }
+
+        if (typeOfGun == 3)
+        {
+            choosenGun = 8;
+        }
+
+        //Assault Rifles
+        if (typeOfGun == 4)
+        {
+            choosenGun = 12;
+        }
+
+        if (typeOfGun == 5)
+        {
+            choosenGun = 16;
+        }
+
+        if (typeOfGun == 6)
+        {
+            choosenGun = 20;
+        }
+
+        //Shotguns
+        if (typeOfGun == 7)
+        {
+            choosenGun = 24;
+        }
+
+        if (typeOfGun == 8)
+        {
+            choosenGun = 28;
+        }
+
+        if (typeOfGun == 9)
+        {
+            choosenGun = 32;
+        }
+
+        //Sniper Rifles
+        if (typeOfGun == 10)
+        {
+            choosenGun = 36;
+        }
+
+        if (typeOfGun == 11)
+        {
+            choosenGun = 40;
+        }
+
+        if (typeOfGun == 12)
+        {
+            choosenGun = 44;
+        }
+
+        return choosenGun;
+    }
+
+    public int uncommonSelector(int typeOfGun)
+    {
+        int choosenGun = 0;
+
+        //Pistol
+        if (typeOfGun == 1)
+        {
+            choosenGun = 1;
+        }
+
+        if (typeOfGun == 2)
+        {
+            choosenGun = 5;
+        }
+
+        if (typeOfGun == 3)
+        {
+            choosenGun = 9;
+        }
+
+        //Assault Rifles
+        if (typeOfGun == 4)
+        {
+            choosenGun = 13;
+        }
+
+        if (typeOfGun == 5)
+        {
+            choosenGun = 17;
+        }
+
+        if (typeOfGun == 6)
+        {
+            choosenGun = 21;
+        }
+
+        //Shotguns
+        if (typeOfGun == 7)
+        {
+            choosenGun = 25;
+        }
+
+        if (typeOfGun == 8)
+        {
+            choosenGun = 29;
+        }
+
+        if (typeOfGun == 9)
+        {
+            choosenGun = 33;
+        }
+
+        //Sniper Rifles
+        if (typeOfGun == 10)
+        {
+            choosenGun = 37;
+        }
+
+        if (typeOfGun == 11)
+        {
+            choosenGun = 41;
+        }
+
+        if (typeOfGun == 12)
+        {
+            choosenGun = 45;
+        }
+
+        return choosenGun;
+    }
+
+    public int rareSelector(int typeOfGun)
+    {
+        int choosenGun = 0;
+
+        //Pistol
+        if (typeOfGun == 1)
+        {
+            choosenGun = 2;
+        }
+
+        if (typeOfGun == 2)
+        {
+            choosenGun = 6;
+        }
+
+        if (typeOfGun == 3)
+        {
+            choosenGun = 10;
+        }
+
+        //Assault Rifles
+        if (typeOfGun == 4)
+        {
+            choosenGun = 14;
+        }
+
+        if (typeOfGun == 5)
+        {
+            choosenGun = 18;
+        }
+
+        if (typeOfGun == 6)
+        {
+            choosenGun = 22;
+        }
+
+        //Shotguns
+        if (typeOfGun == 7)
+        {
+            choosenGun = 26;
+        }
+
+        if (typeOfGun == 8)
+        {
+            choosenGun = 30;
+        }
+
+        if (typeOfGun == 9)
+        {
+            choosenGun = 34;
+        }
+
+        //Sniper Rifles
+        if (typeOfGun == 10)
+        {
+            choosenGun = 38;
+        }
+
+        if (typeOfGun == 11)
+        {
+            choosenGun = 42;
+        }
+
+        if (typeOfGun == 12)
+        {
+            choosenGun = 46;
+        }
+
+        return choosenGun;
+    }
+
+    public int epicSelector(int typeOfGun)
+    {
+        int choosenGun = 0;
+
+        //Pistol
+        if (typeOfGun == 1)
+        {
+            choosenGun = 3;
+        }
+
+        if (typeOfGun == 2)
+        {
+            choosenGun = 7;
+        }
+
+        if (typeOfGun == 3)
+        {
+            choosenGun = 11;
+        }
+
+        //Assault Rifles
+        if (typeOfGun == 4)
+        {
+            choosenGun = 15;
+        }
+
+        if (typeOfGun == 5)
+        {
+            choosenGun = 19;
+        }
+
+        if (typeOfGun == 6)
+        {
+            choosenGun = 23;
+        }
+
+        //Shotguns
+        if (typeOfGun == 7)
+        {
+            choosenGun = 27;
+        }
+
+        if (typeOfGun == 8)
+        {
+            choosenGun = 31;
+        }
+
+        if (typeOfGun == 9)
+        {
+            choosenGun = 35;
+        }
+
+        //Sniper Rifles
+        if (typeOfGun == 10)
+        {
+            choosenGun = 39;
+        }
+
+        if (typeOfGun == 11)
+        {
+            choosenGun = 43;
+        }
+
+        if (typeOfGun == 12)
+        {
+            choosenGun = 47;
+        }
+
+        return choosenGun;
+    }
+
+    public int legendarySelector(int typeOfGun)
+    {
+        int choosenGun = 0;
+
+        if ((typeOfGun >= 1) && (typeOfGun <= 3))
+        {
+            choosenGun = 48;
+        }
+
+        if ((typeOfGun >= 4) && (typeOfGun <= 6))
+        {
+            choosenGun = 49;
+        }
+
+        if ((typeOfGun >= 7) && (typeOfGun <= 9))
+        {
+            choosenGun = 50;
+        }
+
+        if ((typeOfGun >= 10) && (typeOfGun <= 11))
+        {
+            choosenGun = 51;
+        }
+
+        return choosenGun;
+    }
 }
+
+
