3719820e06ccc85eec8814848148576e9d72c455
Marson Beluso <wnp5030@aut.ac.nz>
2022-10-24 20:37:54 +1300

    Audio Setting fixed
    it can transfer to other scenes due to it's easy implementation to carry out. Only issue is that it's not going to change values when coming back to the menu scene

--- PermaGreed/Assets/Scripts/Menu/AudioOptions.cs
+++ PermaGreed/Assets/Scripts/Menu/AudioOptions.cs
@@ -14,12 +14,18 @@ public class AudioOptions : MonoBehaviour
     void Awake()
     {
         musicOption.onValueChanged.AddListener(ChangeMusic);
+        soundOption.onValueChanged.AddListener(ChangeSound);
     }
 
     void ChangeMusic(float musicValue)
     {
         mixer.SetFloat(MUSIC_MIXER, Mathf.Log10(musicValue) * 20);
         //Keep in mind, AudioMixer is in logarithmic (0.0001 to 10,000) while slider is linear (-5 to 5)
+    }
+    
+    void ChangeSound(float musicValue)
+    {
+        mixer.SetFloat(SOUND_MIXER, Mathf.Log10(musicValue) * 20);
     }
 
 }
